8f2a518182ae4b113ac69a05644b09ab3bbb86b8
Donald Rossberg <donaldrossberg@live.com>
2018-01-27 02:01:24 -0600

    Added C# content - GUI project is still incomplete.

---
+++ C#/GUI/HashItOut/HashItOut/HashItOut/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HashItOut.ViewModels;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.ComponentModel;
+using System.Security.Cryptography;
+
+namespace HashItOut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HashViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new HashViewModel();
+            DataContext = _viewModel;
+        }
+
+        // Selects the source files to on board
+        private void AddSrcFiles_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel = new HashViewModel();
+
+            System.Windows.Forms.OpenFileDialog SelectFile = new System.Windows.Forms.OpenFileDialog
+            {
+                InitialDirectory = "C:\\",
+                Filter = "All Files (*.*)|*.*",
+                Title = "Select A File",
+                Multiselect = true
+            };
+            SelectFile.ShowDialog();
+
+            if (SelectFile.FileNames != null && SelectFile.FileNames.Length > 0)
+            {
+                foreach (string fn in SelectFile.FileNames)
+                {
+                    _viewModel.SourceFiles.Add(fn);
+                    SourceFiles.ItemsSource = _viewModel.SourceFiles;
+                }
+            }
+        }
+
+        private void GetHashValues_Click(object sender, RoutedEventArgs e)
+        {
+            BeginOperation();
+        }
+
+
+        private void BeginOperation()
+        {
+            if (_viewModel.Results.Count > 0)
+            {
+                _viewModel.Results = new List<string>();
+            }
+
+            // if the MD5 option is selected
+            if (_viewModel.MD5Selected)
+            {
+                foreach (string source in _viewModel.SourceFiles)
+                {
+                    using (MD5 _md5 = MD5.Create())
+                    {
+                        using (var stream = File.OpenRead(source))
+                        {
+                            _viewModel.Results.Add(BitConverter.ToString(_md5.ComputeHash(stream)).Replace("-", "‌​").ToLower());
+                        }
+                    }
+                }
+            }
+
+            if (_viewModel.SHA1Selected)
+            {
+                foreach (string source in _viewModel.SourceFiles)
+                {
+                    using (var cryptoProvider = new SHA1CryptoServiceProvider())
+                    {
+                        using (var stream = File.OpenRead(source))
+                        {
+                            _viewModel.Results.Add(BitConverter.ToString(cryptoProvider.ComputeHash(stream)).Replace("-", "‌​").ToLower());
+                        }
+                    }
+                }
+            }
+        }
+
+        // UI Controls
+        private void CloseApplication_Click(Object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Allows click + drag anywhere on form
+        private void title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            LayoutRoot.Visibility = Visibility.Hidden;
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ C#/GUI/HashItOut/HashItOut/HashItOut/ViewModels/HashViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashItOut.ViewModels
+{
+    class HashViewModel : INotifyPropertyChanged
+    {
+        public List<string> SourceFiles
+        {
+            get { return _sourceFiles; }
+            set { _sourceFiles = value; OnPropertyChanged("SourceFiles"); }
+        }
+        private List<string> _sourceFiles = new List<string>();
+
+        public List<string> Results
+        {
+            get { return _results; }
+            set { _results = value; OnPropertyChanged("Results"); }
+        }
+        private List<string> _results = new List<string>();
+
+        public bool SHA1Selected
+        {
+            get { return _sha1; }
+            set { _sha1 = value; OnPropertyChanged("SHA1"); }
+        }
+        private bool _sha1 = true;
+
+        public bool MD5Selected
+        {
+            get { return _md5; }
+            set { _md5 = value; OnPropertyChanged("MD5Selected"); }
+        }
+        private bool _md5 = true;
+
+        #region Interface Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
